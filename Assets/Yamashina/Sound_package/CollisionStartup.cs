80a812593267477fc3cbde3a79725ce344f4c647
maho0628 <maho06282001@gmail.com>
2023-12-09 19:43:33 +0900

    オプション画面作業取り組み

---
+++ Assets/Yamashina/Sound_package/CollisionStartup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionStartup : MonoBehaviour
+{
+    Image image;
+
+    void Awake()
+    {
+        image = GetComponent<Image>();
+        if (image != null)
+        {
+            image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+        }
+    }
+}
