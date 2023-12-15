3eff67d1d57ff8fda28f357f695ee9b41057ffea
maho0628 <maho06282001@gmail.com>
2023-12-15 15:19:01 +0900

    オプションプレハブ周り作業
    オプションプレハブをinstantiate,destoryともに完了しました。
    SEの音量調節が出来ておらず、タイトル画面画面からオプションへ移動した際の二重に音が聞こえてしまっています。

---
+++ Assets/Yamashina/Scripts/TitletoOption.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitletoOption : MonoBehaviour
+{
+    [SerializeField] public GameObject prefab;
+
+    public void OnClick()
+    {
+
+        Instantiate(prefab);
+    }
+}
