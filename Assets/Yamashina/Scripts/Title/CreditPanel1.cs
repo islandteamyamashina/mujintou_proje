5f9f39b2e8557a9bd76ebe195479da0480feea5b
maho0628 <maho06282001@gmail.com>
2023-12-19 23:53:55 +0900

    オプション画面追加

--- Assets/Yamashina/Scripts/Title/CreditPanel1.cs
+++ Assets/Yamashina/Scripts/Title/CreditPanel1.cs
@@ -6,22 +6,35 @@ public class CreditPanel1 : MonoBehaviour
 {
     public GameObject mainPanel;
     public GameObject subPanel;
+    public GameObject CreditPanel;
 
     void Start()
     {
         mainPanel.SetActive(true);
         subPanel.SetActive(false);
+        CreditPanel.SetActive(false);
+
     }
 
     public void MainView()
     {
         mainPanel.SetActive(true);
         subPanel.SetActive(false);
+        CreditPanel.SetActive(false);
+
     }
 
     public void SubView()
     {
         mainPanel.SetActive(false);
         subPanel.SetActive(true);
+        CreditPanel.SetActive(false);
+
+    }
+    public void CreditView() 
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(false);
+        CreditPanel.SetActive(true);
     }
 }
