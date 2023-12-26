d7594638de4cafa73e86b19f9c2c13f75e08ff62
maho0628 <maho06282001@gmail.com>
2023-12-26 13:21:59 +0900

    一度作業中止

--- Assets/Yamashina/Scripts/Inventory/Craft/CraftList.cs
+++ Assets/Yamashina/Scripts/Inventory/Craft/CraftList.cs
@@ -3,16 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CraftList : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{//
+ //
+ //  関数として、クラフトに必要なアイテムかどうかを判定するクラスです。
+ //  同時にCraftItemIDクラスも管理しています。
+ //
 
-    // Update is called once per frame
-    void Update()
+    [Header("クラフトした結果のアイテム配列"), SerializeField]
+    CraftItemID[] cpCraftPrefab;
+    /*  
+    public GameObject CraftItem(Item1 _fItem, Item1 _sItem)
     {
-        
-    }
+        //  引数のItemクラスから、そのアイテムのIDを取得
+            int getFid = _fItem.GetItemID();
+              int getSid = _sItem.GetItemID();
+
+              //  for文で取得したIDの組み合わせでクラフトできるものがあるかチェック
+              for (int i = 0; i < cpCraftPrefab.Length; i++)
+              {
+                  //  取得したIDとクラフト表のIDを照らし合わせる
+                  if (getFid == cpCraftPrefab[i].GetID(true) && getSid == cpCraftPrefab[i].GetID(false))
+                  {
+                      return Instantiate(cpCraftPrefab[i].GetCraftItem());
+                  }
+                  //  反転して入れていても対応する用
+                  else if (getFid == cpCraftPrefab[i].GetID(false) && getSid == cpCraftPrefab[i].GetID(true))
+                  {
+                      return Instantiate(cpCraftPrefab[i].GetCraftItem());
+                  }
+              }
+              Debug.Log("ID:" + getFid + "-" + getSid + "のクラフトアイテムは存在しません");
+              return null;
+
+          }
+          */
 }
+
