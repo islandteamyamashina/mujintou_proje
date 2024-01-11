0dde2d59263182ce4faa8a54ead3d19ec0116277
maho0628 <maho06282001@gmail.com>
2024-01-11 19:11:05 +0900

    拠点周り色々試行錯誤

--- Assets/Yamashina/Scripts/Kyoten/KyotenToOtherspace.cs
+++ Assets/Yamashina/Scripts/Kyoten/KyotenToOtherspace.cs
@@ -1,18 +1,109 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KyotenToOtherspace : MonoBehaviour
+public class KyotenToOtherspace : CreditPanel1
 {
+    public GameObject InventoryPanel;
+    public GameObject TakibiPanel;
+    public GameObject WakimizuPanel;
+    public GameObject cookingPanel;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainPanel.SetActive(true);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(false);
+        cookingPanel.SetActive(false);
     }
 
     // Update is called once per frame
-    void Update()
+    public override void MainView()
     {
-        
+        mainPanel.SetActive(true);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(false);
+        cookingPanel.SetActive(false);
+
+    }
+
+    public override void SubView()
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(true);
+        OptionPanel.SetActive(false);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(false);
+        cookingPanel.SetActive(false);
+
+
+    }
+    public override void CreditView()
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(true);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(false);
+        cookingPanel.SetActive(false);
+
+    }
+
+    public void InventoryView()
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        cookingPanel.SetActive(false);
+        InventoryPanel.SetActive(true);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(false);
+
+    }
+    public void WakimizuView()
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        cookingPanel.SetActive(false);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(true);
+
+    }
+    public void TakibiView()
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        cookingPanel.SetActive(false);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(true);
+        WakimizuPanel.SetActive(false);
+
+    }
+    public void CookingView()
+    {
+        mainPanel.SetActive(false);
+        subPanel.SetActive(false);
+        OptionPanel.SetActive(false);
+        cookingPanel.SetActive(true);
+        InventoryPanel.SetActive(false);
+        TakibiPanel.SetActive(false);
+        WakimizuPanel.SetActive(false);
+
     }
 }
+
+
