109774055802afa6bf72b186908ebce453b3e0af
maho0628 <maho06282001@gmail.com>
2024-01-25 14:39:01 +0900

    焚火完了

--- Assets/Yamashina/Scripts/Kyoten/ImageChange_kyoten.cs
+++ Assets/Yamashina/Scripts/Kyoten/ImageChange_kyoten.cs
@@ -11,53 +11,61 @@ public class ImageChange_kyoten : Image_Method
     public KyotenToOtherspace kyotenToOtherspace;
 
     public Image TakibiImage;
-    [SerializeField] public Sprite image;
-    [SerializeField] public Sprite Image2;
+    [SerializeField] public Sprite Takibi_off;
+    [SerializeField] public Sprite Takibi_on;
     void Start()
     {
-
+        
         image_num = 0;
         if (kyotenToOtherspace.takibi <= 0)
         {
-            TakibiImage.sprite = Image2;
+            TakibiImage.sprite = Takibi_off;
         }
         else
         {
-            TakibiImage.sprite = image;
+            TakibiImage.sprite = Takibi_on;
         }
         // PutImage(image);
     }
     public void ImageChange()
     {
-        if (kyotenToOtherspace.takibi_f==false)
+        Debug.Log("ƒ{ƒ^ƒ“‰Ÿ‚µ‚½");
+        Debug.Log(kyotenToOtherspace.takibi);
+        if (kyotenToOtherspace.takibi_f == false)
         {
-            if(Input.GetMouseButtonDown(0)) { 
-            if (image_num == 0 && kyotenToOtherspace.takibi <= 0)
+            Debug.Log("ƒ{ƒ^ƒ“‰Ÿ‚µ‚½‚Q");
+            int rand_num;
+            rand_num = Random.Range(0, 3) ;
+            if (rand_num == 3) rand_num = 2;
+            if (rand_num == 0)
             {
-                TakibiImage.sprite = Image2;
-                //PutImage(Image2);
-                //Remove_Image(image);
-                image_num = 1; 
-                kyotenToOtherspace.takibi_f = true;
-                Debug.Log("¬Œ÷");
+                if (image_num == 0 && kyotenToOtherspace.takibi <= 0)
+                {
+                    TakibiImage.sprite = Takibi_on;
+                    //PutImage(Image2);
+                    //Remove_Image(image);
+                    image_num = 1;
+                    kyotenToOtherspace.takibi = 100;
+                    kyotenToOtherspace.takibi_f = true;
+                    Debug.Log("¬Œ÷");
 
 
+                }
             }
+            else {
+             //if (image_num == 1 && kyotenToOtherspace.takibi > 0)
+             //   {
+                    TakibiImage.sprite = Takibi_off;
+                    //PutImage(image);
+                    //Remove_Image(Image2);
+                    image_num = 0;
+                    Debug.Log(kyotenToOtherspace.takibi);
+                    Debug.Log("Ž¸”s");
 
-            else if(image_num == 1 && kyotenToOtherspace.takibi > 0)
-            {
-                TakibiImage.sprite = image;
-                //PutImage(image);
-                //Remove_Image(Image2);
-                image_num = 0;
-                kyotenToOtherspace.takibi = 100;
-                Debug.Log(kyotenToOtherspace.takibi);
-                Debug.Log("Ž¸”s");
-
+                //}
+                //Debug.Log(kyotenToOtherspace.takibi);
             }
-
         }
-            }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             kyotenToOtherspace.takibi = 0;
