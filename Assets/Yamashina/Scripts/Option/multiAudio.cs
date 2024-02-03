47365a827af4c229b871f9897b408f75c2e83c43
maho0628 <maho06282001@gmail.com>
2024-02-03 19:18:47 +0900

    タイトルハイライト

--- Assets/Yamashina/Scripts/Option/multiAudio.cs
+++ Assets/Yamashina/Scripts/Option/multiAudio.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Recorder;
 using UnityEngine;
 
 public class multiAudio : MonoBehaviour
 {
+    bool f = false;
+
       [SerializeField] private AudioSource a;//AudioSource型の変数aを宣言 使用するAudioSourceコンポーネントをアタッチ必要
 
         [SerializeField] private AudioClip b1;//AudioClip型の変数b1を宣言 使用するAudioClipをアタッチ必要
@@ -15,11 +19,11 @@ public class multiAudio : MonoBehaviour
             a.PlayOneShot(b1);
         }
 
-        //自作の関数2
-        public void SE2()
-        {
+    //自作の関数2
+    public void SE2()
+    {
             a.PlayOneShot(b2);
-        }
+    }
 
     public void SE3()
     {
