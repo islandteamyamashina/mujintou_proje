e580fe3bb553d971a89938950ba1f41826c37419
maho0628 <maho06282001@gmail.com>
2024-02-15 23:45:44 +0900

    0215

---
+++ Assets/Yamashina/Scripts/Cursol.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cursol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+
+    public Texture2D handCursor;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Cursor.SetCursor(handCursor, new Vector2(handCursor.width / 2, handCursor.height / 2), CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 TargetPos = Camera.main.ScreenToWorldPoint(eventData.position);
+        TargetPos.z = 0;
+        transform.position = TargetPos;
+    }
+}
+
