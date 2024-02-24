9640fc770017ff388b54edf91661dc3b8746d927
Route-Paradise <lappyparadise1341@gmail.com>
2024-02-24 17:55:47 +0900

    文字送り進化

--- Assets/Tani/Scripts/TextControl.cs
+++ Assets/Tani/Scripts/TextControl.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class TextControl : MonoBehaviour
 {
+    [SerializeField, Header("テキストを表示するオブジェクト")]
+    GameObject TextObject;
     Text text;
-    string str = "123456789ああああああああああaaaaaaaaaaaaaaaaa";
-    float interval = 0.1f;
-    int strCount = 0;
+    
+
+    [SerializeField,Header("文字の増やす間隔")]
+    float interval = 0.15f;
+
+    [SerializeField]List<string> strs;
+    int str_Count = 0;
+    int str_page = 0;
     float time_sum = 0;
     void Start()
     {
         text = GetComponent<Text>();
+        EventTrigger eventTrigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener((data) => { OnClick(); });
+        eventTrigger.triggers.Add(entry);
     }
 
     // Update is called once per frame
@@ -22,8 +35,25 @@ public class TextControl : MonoBehaviour
         if(time_sum > interval)
         {
             time_sum = 0;
-            strCount++;
+            str_Count++;
         }
-        text.text = str[..strCount];
+        text.text = strs[str_page][..str_Count];
+
+
+    }
+    void OnClick()
+    {
+        
+        str_page++;
+        if(str_page != strs.Count)
+        {
+            str_Count = 0;
+            time_sum = 0;
+        }
+        else
+        {
+            str_page = strs.Count - 1;
+        }
+        
     }
 }
