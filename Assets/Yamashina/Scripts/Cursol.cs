749ff3ef07069a6b373ef1a2b7c2ba414db2097a
maho0628 <maho06282001@gmail.com>
2024-03-02 23:39:32 +0900

    カーソル

--- Assets/Yamashina/Scripts/Cursol.cs
+++ Assets/Yamashina/Scripts/Cursol.cs
@@ -7,9 +7,16 @@ using UnityEngine.EventSystems;
 public class Cursol : MonoBehaviour
 {
     // Start is called before the first frame update
-   
 
-    
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+        }
+
+        }
         public Texture2D cursor;
 
         public void OnMouseEnter()
@@ -30,6 +37,10 @@ public class Cursol : MonoBehaviour
         Vector3 TargetPos = Camera.main.ScreenToWorldPoint(objectScreenPoint);
         TargetPos.z = 0;
         transform.position = TargetPos;
+    }
+    public void OnDrop()
+    {
+        Destroy(gameObject);
     }
 }
 
