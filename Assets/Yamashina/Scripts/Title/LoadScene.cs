3a746654c514c856dece829ff56e9d7239a3fcb9
maho0628 <maho06282001@gmail.com>
2024-03-17 22:20:07 +0900

    0317

--- Assets/Yamashina/Scripts/Title/LoadScene.cs
+++ Assets/Yamashina/Scripts/Title/LoadScene.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadScene : MonoBehaviour
 {
     
   
     [SerializeField] Fade fade;
+    [SerializeField] Text Text;
 
-
+    [SerializeField]Button continueButton;
     [SerializeField] CreditPanel1 CreditPanel1;
     // Update is called once per frame
     public void Text_of_each_places(int num=0)
@@ -37,7 +39,24 @@ public class LoadScene : MonoBehaviour
         fade.feadout_f = true;
 
     }
-  
+    public void Continue(int num)
+    {
+        if (!DataManager.Instance.DoesSaveExist())
+        {
+            continueButton.interactable = false;
+            string colorString = "#4D4D4D"; // ÔF‚Ì16i”•¶š—ñ
+            Color newColor;
+            ColorUtility.TryParseHtmlString(colorString, out newColor); // V‚µ‚­Color‚ğì¬
+            Text.color = newColor;
+        }
+        else
+        {
+            DataManager.Instance.Load();
+            fade.scene_name_num = num;
+            fade.feadout_f = true;
+
+        }
+    }
 
 }
 
