53bd33eee8e15011e1642ba626b48201f181848d
Sakamoto0722 <teruteru.0722.s@gmail.com>
2024-03-20 16:31:54 +0900

    3/20作業分

--- Assets/sakamoto/Scrips/Eventf_Manager.cs
+++ Assets/sakamoto/Scrips/Eventf_Manager.cs
@@ -8,14 +8,27 @@ public class Event_Manage : MonoBehaviour
     [SerializeField] public NewItem[] newItem;
     public int start_event_num;
     public int now_event_num;
+    [SerializeField] int start_area;
     // Start is called before the first frame update
     void Awake()
-    {     
-        start_event_num = Random.Range(-1, 2) + 1;
-        //Debug.Log("start_num‚Í" + start_event_num + "‚Å‚·");
-        if (start_event_num > 2)
+    {
+        if (start_area == 0)
         {
-            start_event_num = 2;
+            start_event_num = Random.Range(-1, 2) + 1;
+            //Debug.Log("start_num‚Í" + start_event_num + "‚Å‚·");
+            if (start_event_num > 2)
+            {
+                start_event_num = 2;
+            }
+        }
+        if (start_area == 1)
+        {
+            start_event_num = Random.Range(6, 9) + 1;
+            //Debug.Log("start_num‚Í" + start_event_num + "‚Å‚·");
+            if (start_event_num > 9)
+            {
+                start_event_num = 9;
+            }
         }
         now_event_num = start_event_num;
         Debug.Log(eventDatas[start_event_num].Main_Text);
