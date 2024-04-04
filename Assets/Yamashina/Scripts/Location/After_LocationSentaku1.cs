6c20cd23f7d9943da268f5dd8a962d42fba10293
maho0628 <maho06282001@gmail.com>
2024-04-05 04:38:25 +0900

    ロケーション周りプレハブ整理完了（色々インスペクターで設定する必要あり

--- Assets/Yamashina/Scripts/Location/After_LocationSentaku1.cs
+++ Assets/Yamashina/Scripts/Location/After_LocationSentaku1.cs
@@ -10,8 +10,7 @@ public class After_LocationSentaku1 : MonoBehaviour
     public GameObject newPanel; // 新しいUIパネルへの参照
                                 // public GameObject text;
                                 // public KyotenToOtherspace kyotenToOtherspace;
-    [SerializeField]
-    string sceneName;
+    
     [SerializeField] Fade fade;
     [SerializeField] EventTrigger eventTriggerKawabe;
     [SerializeField] EventTrigger eventTrigger_Forest;
@@ -34,10 +33,16 @@ public class After_LocationSentaku1 : MonoBehaviour
         newPanel.SetActive(false); // パネルを表示する
         eventTriggerKawabe.enabled = true;
         eventTrigger_Doukutu.enabled = true;
+        eventTrigger_Forest.enabled = true;
+        eventTrigger_Kaigan.enabled = true; 
+        eventTrigger_Kakou.enabled = true;  
+        eventTrigger_Sangaku.enabled = true;    
         button_Kawabe.enabled = true;
         button_Forest.enabled = true;
         button_Kaigan.enabled = true;
         button_Kakou.enabled = true;
+        button_Doukutu.enabled = true;  
+        button_Sangaku.enabled =true;   
 
 
     }
@@ -47,7 +52,19 @@ public class After_LocationSentaku1 : MonoBehaviour
 
         ShowNewPanel(); // 新しいUIパネル(After_locationsentakuパネルを表示
                         //  text.SetActive(true);
-
+        eventTriggerKawabe.enabled =false;
+        eventTrigger_Doukutu.enabled = false;
+        eventTrigger_Forest.enabled = false;
+        eventTrigger_Kaigan.enabled = false;
+        eventTrigger_Kakou.enabled = false;
+        eventTrigger_Sangaku.enabled = false;
+        //ボタン関連
+        button_Kawabe.enabled = false;
+        button_Forest.enabled = false;
+        button_Kaigan.enabled = false;
+        button_Kakou.enabled = false;
+        button_Doukutu.enabled = false;
+        button_Sangaku.enabled = false;
     }
 
 
@@ -62,11 +79,24 @@ public class After_LocationSentaku1 : MonoBehaviour
             case 1: //押されたボタンのUnity側で設定された引数が1のだったとき
                 Debug.Log("Aのボタンが押されたよ");
                 fade.feadout_f = true;
+                fade.scene_name_num = num;
 
                 break;
             case 2: //押されたボタンのUnity側で設定された引数が2のだったとき
                 Debug.Log("Bのボタンが押されたよ");
                 newPanel.SetActive(false);
+                eventTriggerKawabe.enabled = true;
+                eventTrigger_Doukutu.enabled = true;
+                eventTrigger_Forest.enabled = true;
+                eventTrigger_Kaigan.enabled = true;
+                eventTrigger_Kakou.enabled = true;
+                eventTrigger_Sangaku.enabled = true;
+                button_Kawabe.enabled = true;
+                button_Forest.enabled = true;
+                button_Kaigan.enabled = true;
+                button_Kakou.enabled = true;
+                button_Doukutu.enabled = true;
+                button_Sangaku.enabled = true;
                 //  kyotenToOtherspace.MainView();
 
                 break;
@@ -80,6 +110,7 @@ public class After_LocationSentaku1 : MonoBehaviour
     public void ShowNewPanel()
     {
         newPanel.SetActive(true); // パネルを表示する
+       
 
     }
 }
