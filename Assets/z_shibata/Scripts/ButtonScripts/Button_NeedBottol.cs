03f236c0ab46843e8bf71135533bec5d4267b4f0
Junya-Shibata08 <vant.shibata@gmail.com>
2024-04-11 14:59:01 +0900

    森イベント追加＋谷フォルダのボタンの名前を変更しました

--- Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
+++ Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
@@ -10,6 +10,7 @@ public class Button_NeedBottol : MonoBehaviour
     void Start()
     {
         button = GetComponent<Button>();
+        PlayerInfo.Instance.Inventry.UseItem(Items.Item_ID.item_mat_bottle);
         button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_bottle) >= 1;
     }
 
