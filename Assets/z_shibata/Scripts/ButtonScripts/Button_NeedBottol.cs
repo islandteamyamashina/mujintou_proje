19f2bbbca671c02792487341a3178f1afa86b436
Junya-Shibata08 <vant.shibata@gmail.com>
2024-04-13 17:58:32 +0900

    現状可能なアイテム設定＋川辺イベント追加

--- Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
+++ Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
@@ -10,8 +10,10 @@ public class Button_NeedBottol : MonoBehaviour
     void Start()
     {
         button = GetComponent<Button>();
-        PlayerInfo.Instance.Inventry.UseItem(Items.Item_ID.item_mat_bottle);
         button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_bottle) >= 1;
+
+        gameObject.GetComponent<Button>().onClick.AddListener(() =>
+        { PlayerInfo.Instance.Inventry.UseItem(Items.Item_ID.item_mat_bottle); });
     }
 
     
