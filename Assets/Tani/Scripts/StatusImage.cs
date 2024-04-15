cbdfa07aa8200baa69d498ae860f5a68e38ac1d4
Route-Paradise <lappyparadise1341@gmail.com>
2024-04-15 15:19:39 +0900

    minor

--- Assets/Tani/Scripts/StatusImage.cs
+++ Assets/Tani/Scripts/StatusImage.cs
@@ -49,17 +49,37 @@ public class StatusImage : MonoBehaviour
         switch (status_type)
         {
             case Status_Type.Health:
-                
+                if (image.fillAmount <= 0.45f)
+                {
+                    image.color = new Color(255 / 255.0f, 56 / 255.0f, 52 / 255.0f);
+
+                }
+                else
+                {
+                    image.color = new Color(143 / 255.0f, 195 / 255.0f, 31 / 255.0f);
+                    if (PlayerInfo.Instance.IsPlayerConditionEqualTo(PlayerInfo.Condition.Poisoned))
+                    {
+                        image.color = new Color(166 / 255.0f, 28 / 255.0f, 195 / 255.0f);
+                    }
+                }
                 break;
             case Status_Type.Hunger:
-                
+                if (image.fillAmount <= 0.45f)
+                {
+                    image.color = new Color(234 / 255.0f, 85 / 255.0f, 20 / 255.0f);
+
+                }
+                else
+                {
+                    image.color = new Color(243 / 255.0f, 152 / 255.0f, 0 / 255.0f);
+                }
 
                 break;
             case Status_Type.Thirst:
                 
                 if (image.fillAmount <= 0.45f)
                 {
-                    image.color = new Color(150 / 255.0f, 85 / 255.0f, 20 / 255.0f);
+                    image.color = new Color(234 / 255.0f, 85 / 255.0f, 20 / 255.0f);
                 
                 }
                 else
