f16d3a377d0b01b791732c0107896ce4cc8e4ab8
maho0628 <maho06282001@gmail.com>
2024-04-23 13:30:37 +0900

    オプション画面タイトルへ戻るボタン修正

--- Assets/Yamashina/Scripts/Title/LoadScene_afterTitle.cs
+++ Assets/Yamashina/Scripts/Title/LoadScene_afterTitle.cs
@@ -7,10 +7,16 @@ using UnityEngine.UI;
 public class LoadScene_afterTitle : MonoBehaviour
 {
     [SerializeField] SceneObject Title;
-    
+    [SerializeField] SwitchActivateSelf switchActivate;
+
+    private void Start()
+    {
+        switchActivate=GameObject.FindAnyObjectByType<SwitchActivateSelf>().GetComponent<SwitchActivateSelf>();
+
+    }
     public void Load()
     {
-
+        switchActivate.SwitchActiveSelf();
         SceneManager.LoadScene(Title);
         //SceneManager.sceneLoaded += OnSceneLoaded;
     }
