da108c6edb94f594c40d3441439aec371f659901
Route-Paradise <lappyparadise1341@gmail.com>
2024-05-12 12:40:29 +0900

    Storage,Craft,Cooking のCloseButton

--- Assets/Tani/Scene/OPED/Scripts/OPUIManager.cs
+++ Assets/Tani/Scene/OPED/Scripts/OPUIManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Opening
 {
     public class OPUIManager : MonoBehaviour
     {
+
         // Start is called before the first frame update
         void Start()
         {
