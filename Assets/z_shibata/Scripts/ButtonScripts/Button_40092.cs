ddb5eeca4e1d8a7f17201d6f59c6f5869f966899
Junya-Shibata08 <vant.shibata@gmail.com>
2024-05-12 18:11:24 +0900

    0512_編集分

--- Assets/z_shibata/Scripts/ButtonScripts/Button_40092.cs
+++ Assets/z_shibata/Scripts/ButtonScripts/Button_40092.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class Button_40092 : MonoBehaviour
 {
     EventSceneControllerBase event_controller;
+
     void Start()
     {
+        event_controller = FindObjectOfType<EventSceneControllerBase>();
         GetComponent<Button>().onClick.AddListener(event_controller.GoVolcano);
+
     }
 }
