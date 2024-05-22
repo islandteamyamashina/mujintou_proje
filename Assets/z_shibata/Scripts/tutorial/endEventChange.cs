26ff68693bdf5a1af65577bc15c91b795872c0ba
Junya-Shibata08 <vant.shibata@gmail.com>
2024-05-23 00:12:16 +0900

    text修正適用

--- Assets/z_shibata/Scripts/tutorial/endEventChange.cs
+++ Assets/z_shibata/Scripts/tutorial/endEventChange.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class endEventChange : MonoBehaviour
 {
     [SerializeField] EventDatas newEndEventData;
     void Start()
     {
-        GameData.FindAnyObjectByType(typeof(EventPanelBase)).GetComponent<EventPanelBase>().endEventData = newEndEventData;
+        GetComponent<Button>().onClick.AddListener(SetEndEventData);
+    }
+
+    void SetEndEventData()
+    {
+        var eventPanelBase = GameData.FindAnyObjectByType(typeof(EventPanelBase)) as EventPanelBase;
+        if (eventPanelBase != null)
+        {
+            eventPanelBase.GetType().GetField("endEventData", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).SetValue(eventPanelBase, newEndEventData);
+        }
+        else
+        {
+            Debug.LogError("EventPanelBase not found");
+        }
     }
 }
