cc445f8ae54ef2f9cd6ea186bf8e7e064f8ccfbd
Route-Paradise <lappyparadise1341@gmail.com>
2024-05-30 12:31:22 +0900

    関係ないとこ修正

--- Assets/Tani/Scripts/SetWeatherIcon.cs
+++ Assets/Tani/Scripts/SetWeatherIcon.cs
@@ -5,15 +5,11 @@ using UnityEngine.UI;
 
 public class SetWeatherIcon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        GetComponent<Image>().sprite = PlayerInfo.Instance.SunnyCloudyRainy[(int)PlayerInfo.Instance.weather];
-    }
+    [SerializeField]
+    ImageViewWithSprites view;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        view.SetImageSprite((int)PlayerInfo.Instance.weather);
     }
 }
