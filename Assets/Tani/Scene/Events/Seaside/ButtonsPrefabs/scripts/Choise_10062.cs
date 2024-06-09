1a55b629a584c055253a01a0f0b0087d1abf6817
Route-Paradise <lappyparadise1341@gmail.com>
2024-06-09 13:11:12 +0900

    宇宙船の残骸ボタン修正

--- Assets/Tani/Scene/Events/Seaside/ButtonsPrefabs/scripts/Choise_10062.cs
+++ Assets/Tani/Scene/Events/Seaside/ButtonsPrefabs/scripts/Choise_10062.cs
@@ -7,11 +7,12 @@ using UnityEngine.UI;
 public class Choise_10062 : MonoBehaviour
 {
     Button button;
-    private void Awake()
+    private void Start()
     {
         button = GetComponent<Button>();
-        button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_DIYknife) >= 1
-            || PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_special_knife) >= 1;
+        button.interactable = button.interactable &&
+            (PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_DIYknife) >= 1
+            || PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_special_knife) >= 1);
         
     }
 
