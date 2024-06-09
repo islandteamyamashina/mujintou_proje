842ef00f21392f3052873d00588fe342361abdd5
Junya-Shibata08 <vant.shibata@gmail.com>
2024-06-09 13:24:00 +0900

    optionとOPエフェクトと渇き効果修正

--- Assets/Tani/Scene/Events/EventPanelBase.cs
+++ Assets/Tani/Scene/Events/EventPanelBase.cs
@@ -344,14 +344,14 @@ public class EventPanelBase : MonoBehaviour
 
         if (info.IsPlayerConditionEqualTo(PlayerInfo.Condition.Hungry))
         {
-            conditionChangeText += $"‹ó•  : ‘Ì—Í{info.Health} Ë {Mathf.Clamp(info.Health - (((100 - info.Hunger) / 4) -20),0,100)}\n";
-            info.Health = Mathf.Clamp(info.Health - (((100 - info.Hunger) / 4) - 20), 0, 100);
+            conditionChangeText += $"‹ó•  : ‘Ì—Í{info.Health} Ë {Mathf.Clamp(info.Health - (((100 - info.Hunger) / 4) -19),0,100)}\n";
+            info.Health = Mathf.Clamp(info.Health - (((100 - info.Hunger) / 4) - 19), 0, 100);
         }
 
         if (info.IsPlayerConditionEqualTo(PlayerInfo.Condition.Thirsty))
         {
-            conditionChangeText += $"Š‰‚« : H—¿{info.Hunger} Ë {Mathf.Clamp(info.Hunger - (((100 - info.Thirst) / 4) -20),0,100)}\n";
-            info.Hunger = Mathf.Clamp(info.Hunger - (((100 - info.Thirst) / 4) - 20),0,100);
+            conditionChangeText += $"Š‰‚« : H—¿{info.Hunger} Ë {Mathf.Clamp(info.Hunger - (((100 - info.Thirst) / 4) -19),0,100)}\n";
+            info.Hunger = Mathf.Clamp(info.Hunger - (((100 - info.Thirst) / 4) - 19),0,100);
         }
 
         event_text_control.AddTextData(conditionChangeText);
