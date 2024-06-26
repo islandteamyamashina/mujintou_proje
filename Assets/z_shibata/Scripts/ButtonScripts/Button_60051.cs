d94a824c378ea49a4b177222a259f2bc5e4d7017
Junya-Shibata08 <vant.shibata@gmail.com>
2024-06-26 19:34:25 +0900

    Update Button_60051.cs

--- Assets/z_shibata/Scripts/ButtonScripts/Button_60051.cs
+++ Assets/z_shibata/Scripts/ButtonScripts/Button_60051.cs
@@ -9,11 +9,17 @@ public class Button_60051 : MonoBehaviour
     void Start()
     {
         button = GetComponent<Button>();
+        //アイテム判別してボタンを消す
         if(PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_water) >= 1 || PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_water2) >= 1)
         {
             button.interactable = true;
         }
+        else if (PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_water) < 1 || PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_water2) < 1)
+        {
+            button.interactable = false;
+        }
 
+        //使ったアイテムを減らす処理
         if (PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_craft_water) >= 1)
         {
             gameObject.GetComponent<Button>().onClick.AddListener(() =>
