d69386168974922a39fe5395e07fa3b316f30149
maho0628 <maho06282001@gmail.com>
2024-07-03 15:55:51 +0900

    ID234修正

--- Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
+++ Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
@@ -7,14 +7,28 @@ using UnityEngine.UI;
 public class Button_NeedBottol : MonoBehaviour
 {
     Button button;
-    void Start()
+    private bool Itemget;
+    private void Start()
     {
+        Itemget = false;
         button = GetComponent<Button>();
-        button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_bottle) >= 1;
-
-        gameObject.GetComponent<Button>().onClick.AddListener(() =>
-        { PlayerInfo.Instance.Inventry.UseItem(Items.Item_ID.item_mat_bottle); });
     }
+        private void Update()
+        {
 
+
+           
+            button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_bottle) >= 1;
+        if(Itemget==false)
+        {
+            gameObject.GetComponent<Button>().onClick.AddListener(() =>
+            { PlayerInfo.Instance.Inventry.UseItem(Items.Item_ID.item_mat_bottle); });
+            Itemget = true; 
+        }
+
+           
+
+
+        }
     
 }
