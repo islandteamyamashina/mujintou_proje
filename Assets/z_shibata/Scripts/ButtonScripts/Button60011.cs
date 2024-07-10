d61a2730312f8793d12937c57b1af02415fa179c
Junya-Shibata08 <vant.shibata@gmail.com>
2024-07-10 12:54:31 +0900

    237,239,241対応

---
+++ Assets/z_shibata/Scripts/ButtonScripts/Button60011.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Button60011 : MonoBehaviour
+{
+    Button button;
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_stone) >= 1;
+    }
+}
