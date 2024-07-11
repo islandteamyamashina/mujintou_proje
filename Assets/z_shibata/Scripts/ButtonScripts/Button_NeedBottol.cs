7458a246ec6297f11c4015fd25da3bc89f9103c3
maho0628 <maho06282001@gmail.com>
2024-07-11 19:29:43 +0900

    ボトル修正

--- Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
+++ Assets/z_shibata/Scripts/ButtonScripts/Button_NeedBottol.cs
@@ -13,28 +13,30 @@ public class Button_NeedBottol : MonoBehaviour
         Itemget = false;
         button = GetComponent<Button>();
     }
-        private void Update()
-        {
+    private void Update()
+    {
 
 
-           
-            button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_bottle) >= 1;
-        if(Itemget==false)
+
+        button.interactable = PlayerInfo.Instance.Inventry.GetItemAmount(Items.Item_ID.item_mat_bottle) >= 1;
+        if (Itemget == false)
         {
             gameObject.GetComponent<Button>().onClick.AddListener(() =>
             { PlayerInfo.Instance.Inventry.UseItem(Items.Item_ID.item_mat_bottle); });
-            if (PlayerInfo.Instance.Inventry.GetNullSlot())
+            if (!PlayerInfo.Instance.Inventry.GetNullSlot())
             {
-                PlayerInfo.Instance.Inventry.GetItem(Items.Item_ID.item_mat_bottle, 1);
+                gameObject.GetComponent<Button>().onClick.AddListener(() => { PlayerInfo.Instance.Inventry.GetItem(Items.Item_ID.item_mat_bottle, 1); });
             }
-            Itemget = true;
-            
-            
-        }
-
-           
-
 
         }
-    
+        Itemget = true;
+
+
+    }
+
+
+
+
 }
+    
+
